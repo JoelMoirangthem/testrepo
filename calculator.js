74dffd838053de1c6b6a5f9c009616af9f7ca201
JoelMoirangthem <joelmoirang@gmail,com>
2024-11-09 20:03:13 +0530

    adding new operation

--- calculator.js
+++ calculator.js
@@ -31,3 +31,36 @@ function multiplyNumbers() {
         document.getElementById("result").innerText="multiplication:" +mmultiplication
     }
 }
+function subNumbers() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        document.getElementById("result").innerText="please enter a valid no:";
+    } else {
+        const minus = num1 - num2;
+        document.getElementById("result").innerText="minus:" +minus
+    }
+}
+function multiplyNumbers() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        document.getElementById("result").innerText="please enter a valid no:";
+    } else {
+        const mmultiplication= num1 * num2;
+        document.getElementById("result").innerText="multiplication:" +mmultiplication
+    }
+}
+function mult() {
+    const num1 = parseInt(document.getElementById("num1").value);
+    const num2 = parseInt(document.getElementById("num2").value);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        document.getElementById("result").innerText="please enter a valid no:";
+    } else {
+        const mmultiplication= num1 // num2;
+        document.getElementById("result").innerText="multiplication:" +mmultiplication
+    }
+}
